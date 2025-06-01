df8783392bda549a6a68e2a69bbba47a57d03741
Maciej <wrobcio789@gmail.com>
2025-06-01 14:21:52 +0200

    [0700] [Java] Search In A Binary Search Tree

---
+++ 0700-search-in-a-binary-search-tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        }
+
+        return root;
+    }
+}
